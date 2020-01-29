201b8c8eca676dcdc751c81018e7047b2d913188
Michal Cieciera <michalcieciera1@gmail.com>
2020-01-29 13:26:19 +0100

    zmiany

--- js/slaider.js
+++ js/slaider.js
@@ -16,15 +16,21 @@ function currentSlide(n) {
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+}
+
+// Cars moving
+
+var buttons = document.querySelectorAll(".buttons button");
+for (i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", function (e) {
+    e.preventDefault();
+
+    var slide = document.querySelector(".cars .main").setAttribute('src', 'anim-assets/cars-' + e.target.dataset.color + '.png');
+  });
 }
